7d50e09c07caa6ec0547468d576cb1236215176e
Sai Harsha Vadde <vadde.sharsha.ece17@itbhu.ac.in>
2020-10-22 10:05:57 +0530

    auto-delete private chat in uvloop-queue

--- django_private_chat/channels_uvloop.py
+++ django_private_chat/channels_uvloop.py
@@ -1,16 +1,19 @@
-import asyncio
 import uvloop
+import asyncio
+
 
 
 def get_new_uvloop_queue():
-    loop = uvloop.new_event_loop()
-    return asyncio.Queue(loop=loop)
+    return asyncio.Queue(loop=uvloop.new_event_loop())
+
 
 
 new_messages = get_new_uvloop_queue()
 users_changed = get_new_uvloop_queue()
+
 online = get_new_uvloop_queue()
 offline = get_new_uvloop_queue()
 check_online = get_new_uvloop_queue()
 is_typing = get_new_uvloop_queue()
+
 read_unread = get_new_uvloop_queue()
